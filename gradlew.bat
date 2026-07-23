c10f495bd21216a23c54bae9e376e4654c0e4e77
rifkey1708-wq <rifkey1708@gmail.com>
2026-07-23 18:46:02 +0700

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,7 @@
+@if "%DEBUG%" == "" @echo off
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
